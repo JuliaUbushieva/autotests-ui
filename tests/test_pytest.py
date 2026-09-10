d1bc53620be0b158263def917133ce2bf257779f
JuliaUbushieva <ubushieva.qa@gmail.com>
2026-09-10 13:14:32 -0400

    First tests

---
+++ tests/test_pytest.py
@@ -0,0 +1,10 @@
+def test_first_try():
+    print("Hello World!")
+
+
+def test_assert_positive_case():  # A new test that verifies a positive case.
+    assert (2 + 2) == 4  # The test is expected to be passed
+
+
+def test_assert_negative_case():  # A new test that checks a negative case.
+    assert (2 + 2) == 5  # The test is expected to be failed
